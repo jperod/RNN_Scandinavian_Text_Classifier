2a67d47d2da44fc75bfdaf08536c5936c13a3ffd
Pedro <p3dror@gmail.com>
2020-12-10 21:50:36 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, jsonify, request
 
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+
+
+
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        # we will get the file from the request
+        file = request.files['file']
+        # convert that to bytes
+        img_bytes = file.read()
+        class_id, class_name = get_prediction(image_bytes=img_bytes)
+        return jsonify({'class_id': class_id, 'class_name': class_name})
+
+if __name__ == '__main__':
+    app.run()
