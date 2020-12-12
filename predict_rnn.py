53c1f892676b392b7fc65485922378b3847db2b8
Pedro <p3dror@gmail.com>
2020-12-12 11:32:34 +0000

    Update predict_rnn.py

--- predict_rnn.py
+++ predict_rnn.py
@@ -18,14 +18,14 @@ Word2Index_w_unk["<UNK>"] = ix_unk
 n_words = len(Word2Index)
 rnn = RNN(len(Word2Index), config["n_hidden"], 3)
 rnn.load_state_dict(torch.load(saved_model_dir))
-all_categories = ["da","no","sv"]
+all_categories = Utils().all_categories
 
 #[p for p in rnn.parameters()]
 
 def predict(input_line, n_predictions=3):
     print('\n> %s' % input_line)
     with torch.no_grad():
-        output = evaluate(SentToTensor(input_line, n_words))
+        output = Utils().evaluate(Utils().SentToTensor(input_line, n_words))
         output = np.squeeze(output.numpy())
         pred_ix = np.argmax(output)
         prediction = all_categories[pred_ix]
