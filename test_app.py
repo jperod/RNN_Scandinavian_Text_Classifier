df419c54184d9647db84e94e81963792111ed7e7
pedro <p3dror@gmail.com>
2020-12-12 15:40:01 +0000

    update

--- test_app.py
+++
@@ -1,6 +0,0 @@
-import requests
-from flask import jsonify
-
-sentence = 'Hej, jeg hedder Pedro og jeg elsker at drikke øl!'
-url_2 = '/'+sentence
-resp = requests.get('http://localhost:5000/predict'+url_2)
